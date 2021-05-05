608e9de8ae9fa94be2923926f95b577b57208ff2
AkhilXavier95 <akhilxavier95@gmail.com>
2021-05-06 00:04:16 +0530

    text change

--- app/javascript/src/components/Dashboard/Tasks/DeleteModal.jsx
+++ app/javascript/src/components/Dashboard/Tasks/DeleteModal.jsx
@@ -10,7 +10,7 @@ export default function DeleteAlert({ onClose, handleDelete }) {
       showFooter
       submitButtonProps={{
         style: "danger",
-        label: "Continue anyway",
+        label: "Delete",
         loading: false,
         onClick: handleDelete,
       }}
