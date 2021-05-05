c2f58ea871939c10d41ea13966745f2128f3e424
AkhilXavier95 <akhilxavier95@gmail.com>
2021-05-06 02:24:41 +0530

    Changed Delete Alert Component

--- app/javascript/src/components/Dashboard/Tasks/DeleteModal.jsx
+++ app/javascript/src/components/Dashboard/Tasks/DeleteModal.jsx
@@ -1,37 +1,21 @@
 import React from "react";
-import { Modal } from "neetoui";
+import { Alert } from "neetoui";
 
 export default function DeleteAlert({ onClose, handleDelete }) {
   return (
-    <Modal
+    <Alert
       isOpen
-      size="small"
-      autoHeight
-      showFooter
+      title="Delete Task"
+      message="Are you sure you want to delete the task?"
+      icon="text-red-500 ri-alarm-warning-fill ri-lg"
+      onClose={onClose}
+      hideConfirmation
+      cancelButtonProps={{
+        onClick: onClose,
+      }}
       submitButtonProps={{
-        style: "danger",
-        label: "Delete",
-        loading: false,
         onClick: handleDelete,
       }}
-      onClose={onClose}
-    >
-      <div className="flex">
-        <div className="flex items-center justify-center flex-shrink-0 w-10 h-10 bg-red-100 rounded-full">
-          <i className="text-red-500 ri-alarm-warning-fill ri-lg"></i>
-        </div>
-
-        <div className="ml-4">
-          <h3 className="mb-2 text-lg font-medium text-gray-700">
-            Delete Task
-          </h3>
-          <div className="text-sm leading-5 text-gray-500">
-            Are you sure you want to delete the Task? All of your data will be
-            permanently removed from our database forever. This action cannot be
-            undone.
-          </div>
-        </div>
-      </div>
-    </Modal>
+    />
   );
 }
