6a3e2dde9c7871dd6896431437dfd17a3a87a837
AkhilXavier95 <akhilxavier95@gmail.com>
2021-05-06 03:30:16 +0530

    removed props passed from alert

--- app/javascript/src/components/Dashboard/Tasks/DeleteModal.jsx
+++ app/javascript/src/components/Dashboard/Tasks/DeleteModal.jsx
@@ -5,11 +5,7 @@ export default function DeleteAlert({ onClose, handleDelete }) {
   return (
     <Alert
       isOpen
-      title="Delete Task"
-      message="Are you sure you want to delete the task?"
-      icon="text-red-500 ri-alarm-warning-fill ri-lg"
       onClose={onClose}
-      hideConfirmation
       cancelButtonProps={{
         onClick: onClose,
       }}
